be33d3587d9de395a69904d6ce00225c288673ee
Amos Kipruto <kiprutoamos44@gmail.com>
2025-02-22 18:06:04 +0300

    Created the first model and made migrations.

--- BLOGWEBSITE/blogweb/models.py
+++ BLOGWEBSITE/blogweb/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class posts(models.Model):
+  title = models.CharField(max_length=100)
+  content = models.CharField(max_length=1000)
+  created_at = models.DateTimeField(default = datetime.now, blank = True)
