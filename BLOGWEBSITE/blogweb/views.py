cf1268cd47c3a01fde43b70db01cdb19cfab706e
Amos Kipruto <kiprutoamos44@gmail.com>
2025-02-23 22:02:59 +0300

    Added another template for displaying a full post .

--- BLOGWEBSITE/blogweb/views.py
+++ BLOGWEBSITE/blogweb/views.py
@@ -4,3 +4,7 @@ from .models import Posts
 def home(request):
   posts = Posts.objects.all()
   return render(request, 'index.html', {'posts': posts})
+
+def post(request, pk):
+  posts = Posts.objects.get(id = pk)
+  return render(request, 'post.html', {'posts': posts})
