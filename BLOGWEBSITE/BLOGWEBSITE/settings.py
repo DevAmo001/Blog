4f641fb40b294cdd5b76923dc22550080742f693
Amos Kipruto <kiprutoamos44@gmail.com>
2025-02-27 00:49:03 +0300

    Added back button and collected static files.

--- BLOGWEBSITE/BLOGWEBSITE/settings.py
+++ BLOGWEBSITE/BLOGWEBSITE/settings.py
@@ -107,11 +107,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # myproject/settings.py
+STATIC_ROOT = BASE_DIR/ 'staticfiles'
+
 STATICFILES_DIRS = [
   BASE_DIR / "static",
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
