35b4214b9c2b45a41fc66d2a078cf5b2f75336f8
Amos Kipruto <kiprutoamos44@gmail.com>
2025-03-15 19:56:29 +0300

    removed unnecessary comment.

--- BLOGWEBSITE/blogweb/models.py
+++ BLOGWEBSITE/blogweb/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from datetime import datetime
 
-# Create your models here.
+
 class Posts(models.Model):
   title = models.CharField(max_length=100)
   content = models.CharField(max_length=100000)
