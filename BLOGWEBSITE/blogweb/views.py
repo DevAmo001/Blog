4fa6f34f249b01f58e3c632e6ecf15d2d8ba55b5
Amos Kipruto <kiprutoamos44@gmail.com>
2025-03-23 14:54:16 +0300

    updated django home template

--- BLOGWEBSITE/blogweb/views.py
+++ BLOGWEBSITE/blogweb/views.py
@@ -3,7 +3,7 @@ from .models import Posts
 
 def home(request):
   posts = Posts.objects.all()
-  return render(request, 'index.html',)
+  return render(request, 'index.html')
 
 def post(request, pk):
   posts = Posts.objects.get(id = pk)
