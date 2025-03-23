427dd3e613202bf6c8152c3190522903de175228
Amos Kipruto <kiprutoamos44@gmail.com>
2025-03-23 15:40:38 +0300

    Routed the blog page

--- BLOGWEBSITE/blogweb/urls.py
+++ BLOGWEBSITE/blogweb/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name = 'home'),
+  path('blog/', views.blog, name = 'blog_page'),
   path('post/<str:pk>', views.post, name = 'post'),
 ]
--- BLOGWEBSITE/blogweb/views.py
+++ BLOGWEBSITE/blogweb/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from .models import Posts
 
 def home(request):
-  posts = Posts.objects.all()
+  # posts = Posts.objects.all()
   return render(request, 'index.html')
+
+def blog(request):
+  # posts = Posts.objects.all()
+  return render(request, 'blog.html')
 
 def post(request, pk):
   posts = Posts.objects.get(id = pk)
