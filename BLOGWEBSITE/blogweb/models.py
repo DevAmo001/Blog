961c1f96deda85f670a4e657ef1350bbc8f3888f
Amos Kipruto <kiprutoamos44@gmail.com>
2025-03-29 15:43:43 +0300

    Added for loop template tag on the blog.html and modified the models.

--- BLOGWEBSITE/blogweb/models.py
+++ BLOGWEBSITE/blogweb/models.py
@@ -2,7 +2,11 @@ from django.db import models
 from datetime import datetime
 
 
-class Posts(models.Model):
+class BlogPosts(models.Model):
   title = models.CharField(max_length=100)
   content = models.CharField(max_length=100000)
-  created_at = models.DateTimeField(default = datetime.now, blank = True)
+  date = models.DateTimeField(default = datetime.now, blank = True)
+  likes = models.IntegerField(default = 0)
+  comments = models.IntegerField(default = 0)
+  author = models.CharField(max_length=100)
+  image = models.ImageField(upload_to='images/', default='images/default.jpg')
