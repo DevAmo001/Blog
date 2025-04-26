b01bed244a7847a467710579f5612d222b3c588a
Amos Kipruto <kiprutoamos44@gmail.com>
2025-04-26 12:36:15 +0300

    Added jazzmin library and fixed the image issue.

--- BLOGWEBSITE/BLOGWEBSITE/settings.py
+++ BLOGWEBSITE/BLOGWEBSITE/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -109,7 +110,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'static'),
+  os.path.join(BASE_DIR, 'blogweb', 'static'),
 ]
 
 # Default primary key field type
@@ -119,3 +120,42 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
+JAZZMIN_SETTINGS = {
+   "show_ui_builder": True,
+
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": True,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": "navbar-secondary",
+    "accent": "accent-primary",
+    "navbar": "navbar-teal navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": True,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-warning",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "materia",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-outline-primary",
+        "secondary": "btn-outline-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
--- BLOGWEBSITE/blogweb/admin.py
+++ BLOGWEBSITE/blogweb/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import BlogPosts
 
-admin.site.register(BlogPosts)
+
+
+class BlogPostsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date', 'likes', 'comments')
+    search_fields = ('title', 'author')
+    list_filter = ('date',)
+    ordering = ('-date',)
+
+
+admin.site.register(BlogPosts, BlogPostsAdmin)
